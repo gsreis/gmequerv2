75048d8806a34e650b44daf327cacda6cc2fc68d
Glauco Reis <glauco@portalbpm.com.br>
2022-07-21 18:09:24 -0300

    Delete show.js

--- commands/show.js
+++
@@ -1,8 +0,0 @@
-function execute(connections, errors, results, properties) { 
-    print("apresentando");
-    results.setPaused(false);
-}
-
-function help() {
-    print("show - apresenta os resultados na tela com o tempo configurado em <statisticTime>");
-}
