e086b0dcacfd3a943365125057b4885a86dae573
Glauco Reis <glauco@portalbpm.com.br>
2022-07-21 17:18:45 -0300

    Update loop.js

--- default/loop.js
+++ default/loop.js
@@ -5,13 +5,49 @@ function execute(connections, errors, results, properties) {
     proparray = properties.getArray();
     sent = 0;
     receiv = 0;
-    for (i=0; i < resarray.length; i++)
-        if (resarray[i].isProducer)
-            sent++;
-        else
-            receiv++;
-    print(" enviados " + sent);
-    print(" recebidos " + receiv);
+    tpssend =0;
+    tpsreceiv = 0;
+    pctSend = 0;
+    pctReceiv = 0;
+    time = 1;
+    
+    mediaTPSSend = 0;
+    mediaTPSReceiv = 0;
+    mediaBytesSend = 0;
+    mediaBytesReceiv = 0;
+        
+    for (i=0; i < resarray.length; i++) {
+        
+        if (resarray[i].isProducer) {
+            pctSend++;
+            sent += resarray[i].messageSize;
+        }
+        else {
+            pctReceiv++;
+            receiv += resarray[i].messageSize;
+        }
+        
+        if ((resarray[i].timestamp - time)  > 1000) {
+            time = resarray[i].timestamp;
+            mediaTPSSend = pctSend;
+            mediaTPSReceiv = pctReceiv;
+            mediaBytesSend = sent;
+            mediaBytesReceiv = receiv;
+
+            pctSend = 0;
+            pctReceiv = 0;
+            sent = 0;
+            receiv = 0;
+        }         
+    }
+    
+    print(" bytes enviados " + mediaBytesSend);
+    print(" bytes recebidos " + mediaBytesReceiv);
+    
+    print(" TPS envio " + mediaTPSSend);
+    print(" TPS recebimento " + mediaTPSReceiv); 
+    
+    print("");
 }
 
 function help() {
